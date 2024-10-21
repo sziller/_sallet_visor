077885c032d1eb3d8cceed5486e46618c6f3f2a1
sziller <szillerke@gmail.com>
2024-10-21 11:11:41 +0200

    RPCHost.py updated

--- SalletNodePackage/RPCHost.py
+++ SalletNodePackage/RPCHost.py
@@ -5,6 +5,7 @@ import inspect
 import logging
 import requests
 import json
+from requests.exceptions import Timeout, HTTPError, RequestException
 
 # Set up a logger
 lg = logging.getLogger(__name__)
@@ -14,68 +15,63 @@ lg.info("START: {:>83} <<<".format("RPCHost.py"))
 class RPCHost(object):
     """=== Class name: RPCHost =========================================================================================
     Object translates calls into RPC commands to an address
-    =================================================================================== by Sziller & internet ==="""
+    ========================================================================= by Sziller & internet & ChatGPT ==="""
     ccn = inspect.currentframe().f_code.co_name  # current class name
     
-    def __init__(self, url: str):
+    def __init__(self, url: str, retries: int = 10, sleep_time: float = 0.5, timeout: int = 10):
         lg.debug("START: {:>85} <<<".format(self.ccn))
         self._session = requests.Session()
         self._url = url
         self._headers = {'content-type': 'application/json'}
+        # --- custom made parameters ---
+        self.retries: int                   = retries
+        self.sleep_time: float              = sleep_time
+        self.timeout: (int, None)           = timeout
 
-    def call(self, rpc_method, *params):
-        """=== Method name: call =======================================================================================
+    def call(self, rpc_method, *params, timeout: (int, None) = None):
+        """=== Instance method =========================================================================================
         Method performs the actual calling of the RPC
         ========================================================================================== by Sziller ==="""
         payload = json.dumps({"method": rpc_method, "params": list(params), "jsonrpc": "2.0"})
-        tries = 10
-        slp = 0.5
         hadconnectionfailures = False
+        timeout = timeout or self.timeout
+        nr_retry = self.retries
+        slp = self.sleep_time
         while True:
             try:
-                response = self._session.post(self._url, headers=self._headers, data=payload)
-            except requests.exceptions.ConnectionError:
-                tries -= 1
-                if tries == 0:
-                    raise Exception('Failed to connect for remote procedure call.')
-                hadfailedconnections = True
-                msg_tmp = "Couldn't connect for remote procedure call! Will try again in {} seconds ({} more tries)"\
-                    .format(slp, tries)
+                response = self._session.post(self._url, headers=self._headers, data=payload, timeout=timeout)
+            except (ConnectionError, Timeout) as e:
+                nr_retry -= 1
+                hadconnectionfailures = True
+                if nr_retry == 0:
+                    msg_final = 'RPC connection failed: \n{}'.format(e)
+                    lg.critical(msg_final)
+                    raise Exception(msg_final)
+                msg_tmp = "Couldn't connect to RPC! Will try again in {} seconds ({} more tries)".format(slp, nr_retry)
                 lg.warning(msg_tmp)
                 time.sleep(slp)
+            except RequestException as e:
+                msg_final = "An unexpected error occurred - Failed RPC call:\n{}".format(e)
+                lg.critical(msg_final)
+                raise Exception(msg_final)
             else:
                 if hadconnectionfailures:
                     msg_tmp = 'Connected for remote procedure call after retry.'
                     lg.warning(msg_tmp)
+                    hadconnectionfailures = False
                 break
         if response.status_code not in (200, 500):
             msg_tmp = 'RPC connection failure: ' + str(response.status_code) + ' ' + response.reason
             lg.critical(msg_tmp)
             raise Exception(msg_tmp)
-        response_json = response.json()
+        try:
+            response_json = response.json()
+        except json.JSONDecodeError as e:
+            msg_final = 'Failed to decode JSON response: ' + str(e)
+            lg.critical(msg_final, exc_info=True)
+            raise Exception(msg_final)
         if 'error' in response_json and response_json['error'] is not None:
             msg_tmp = 'Error in RPC call:\n' + str(response_json['error'])
             lg.critical(msg_tmp)
             raise Exception(msg_tmp)
         return response_json['result']
-
-
-if __name__ == "__main__":
-    pass
-    
-    from dotenv import load_dotenv
-    load_dotenv()
-    from lightning.lightning import LightningRpc
-    import os
-
-    rpcIP = os.getenv("RPC_IP")
-    rpcUser = os.getenv("RPC_USER")
-    rpcPassword = os.getenv("RPC_PSSW")
-    rpcPort = os.getenv("RPC_PORT")
-
-    string = "http://{}:{}@{}:{}".format(rpcUser, rpcPassword, rpcIP, rpcPort)
-    print(string)
-
-    rpc = RPCHost(string)
-    print(rpc.call("getconnectioncount"))
-    
